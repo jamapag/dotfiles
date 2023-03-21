2d13e030047737abf452083441c93d81852e7e05
Maks Makrovets <maks.markovets@gmail.com>
2023-03-22 01:59:16 +0200

    Add vim-tmux-runner and harpoon.

--- config/nvim/lua/plugins/general.lua
+++ config/nvim/lua/plugins/general.lua
@@ -498,5 +498,21 @@ return {
       }
     end
   },
+  { 'christoomey/vim-tmux-runner' },
+  {
+    'ThePrimeagen/harpoon',
+    config = function ()
+      local mark = require("harpoon.mark")
+      local ui = require("harpoon.ui")
+
+      vim.keymap.set("n", "<leader>ha", mark.add_file)
+      vim.keymap.set("n", "<leader>hh", ui.toggle_quick_menu)
+
+      vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
+      vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
+      vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
+      vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
+    end
+  },
 }
 
